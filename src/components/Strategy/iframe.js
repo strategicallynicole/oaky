cd655fd37b35bf92c2d85bc4378376898c806714
Strategically Nicole <36245900+strategicallynicole@users.noreply.github.com>
2021-07-24 11:52:34 -0400

    Update iframe.js

--- src/components/Strategy/iframe.js
+++ src/components/Strategy/iframe.js
@@ -2,12 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import './model.scss'
 
-const Iframe = () =>
+const Iframe = () => {
 // const { iframesrc } = props.iframesrc
-    (
+
         <>
             <div dangerouslySetInnerHTML="{{ __html: `<iframe id='iframe1' scrolling='no' src='/demo.html' width='2200px' />` }}"></div>
         </>
-    )
+};
 
 export default Iframe
