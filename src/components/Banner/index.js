fc6dfb74f86b4d0b2e686c13f25eb60e2cdd606f
Strategically Nicole <36245900+strategicallynicole@users.noreply.github.com>
2021-07-24 09:50:00 -0400

    up

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -16,9 +16,9 @@ import Title from '../Titles/home.js'
 import * as style from './banner.module.scss'
 const Banner = () => (
 
-    <div className="container flex flex-wrap  overflow-hidden ">
+    <div className="container flex flex-wrap overflow-hidden ">
 
-        <div className="my-1 px-10 py-20 w-full  sm:order-last xs:order-last overflow-hidden sm:my-1 sm:px-1 sm:w-full md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
+        <div className="w-full px-10 py-20 my-1 overflow-hidden sm:order-last xs:order-last sm:my-1 sm:px-1 sm:w-full md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
             <div className="lg:mt-20 title wow fadeInLeft"
                 data-wow-delay="300ms"
                 data-wow-duration="1000ms"
@@ -32,7 +32,7 @@ const Banner = () => (
 
         </div>
 
-        <div className="my-1 px-1 w-full overflow-hidden sm:my-1 sm:px-1 sm:w-full md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
+        <div className="w-full px-1 my-1 overflow-hidden sm:my-1 sm:px-1 sm:w-full md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
             <div className="wow fadeInLeft story-image"
                 data-wow-delay="300ms"
                 data-wow-duration="1000ms">
