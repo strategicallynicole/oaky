29b1f6c6d201b7e678d2b6c796a67df9a89984db
Strategically Nicole <36245900+strategicallynicole@users.noreply.github.com>
2021-07-24 15:00:48 -0400

    update

--- src/components/Strategy/iframe.js
+++ src/components/Strategy/iframe.js
@@ -4,7 +4,7 @@ import './model.scss'
 
 const Iframe = () => {
 
-            <div dangerouslySetInnerHTML={{ __html: `<iframe id='iframe1' scrolling='no' src='/demo.html' width='2200px' />` }}></div>
+            <div dangerouslySetInnerHTML={{ __html: `<iframe id='iframe1' scrolling='no' src='/demo.html' />` }}></div>
 
 };
 
