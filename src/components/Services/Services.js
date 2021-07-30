e7aab67932d0011adb33eef59388d9f8d2712911
Strategically Nicole <36245900+strategicallynicole@users.noreply.github.com>
2021-07-29 20:20:13 -0400

    Update Services.js

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -43,14 +43,14 @@ import Title from "../Titles/h2"
       <div className="relative py-16 sm:py-24 lg:py-32">
 
 
-        <div className="max-w-md px-6 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+        <div className="max-w-full px-6 mx-auto text-center sm:max-w-full sm:px-6 lg:px-8 lg:max-w-full">
 
 
           <div className="mt-12">
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 min-h-fit">
+            <div className="grid w-full grid-cols-1 gap-5 sm:grid-cols-1 lg:grid-cols-3">
               {features.map((feature) => (
                 <div key={feature.name} className="pt-6">
-                  <div className="flow-root px-10 pb-8 border border-gray-200 rounded-lg shadow-lg py-30 bg-blurred sm:rounded-3xl sm:p-16 bg-clip-padding backdrop-filter backdrop-blur-xl backdrop-filter-blur">
+                  <div className="flow-root px-10 pb-8 border-gray-200 rounded-lg shadow-lg py-30 bg-blurred sm:rounded-3xl sm:p-16 bg-clip-padding backdrop-filter backdrop-blur-xl backdrop-filter-blur">
                     <div className="-mt-6">
                       <div>
                         <span className="inline-flex items-center justify-center p-3 shadow-lg rounded-xl">
